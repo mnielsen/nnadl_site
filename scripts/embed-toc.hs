4fb0658521e9ca82c0a1f2df5fd3ed5fdcd13318
Takayuki Muranushi <muranushi@gmail.com>
2015-06-12 15:34:25 +0900

    add scripts for managing the layout of the book.

---
+++ scripts/embed-toc.hs
@@ -0,0 +1,21 @@
+
+
+htmlFiles :: FilePath
+htmlFiles = [ "about.html"
+            , "acknowledgements.html"
+            , "bugfinder.html"
+            , "chap1.html"
+            , "chap2.html"
+            , "chap3.html"
+            , "chap4.html"
+            , "chap5.html"
+            , "exercises_and_problems.html"
+            , "faq.html"
+            , "index.html"
+            , "supporters.html"
+            , "translators.html"]
+
+
+main :: IO ()
+main = do
+  print "hi"
